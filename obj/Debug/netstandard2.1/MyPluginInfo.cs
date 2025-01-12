7e7b2c9258497a343152276f465f805f675c31f2
JustSunnit <116322461+Sunnit-M@users.noreply.github.com>
2025-01-12 14:41:39 -0800

    Delete obj directory

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace VirtualInsanity
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "justsunnit.virtual_insanity";
-        public const string PLUGIN_NAME = "VirtualInsanity";
-        public const string PLUGIN_VERSION = "0.1.0";
-    }
-}
