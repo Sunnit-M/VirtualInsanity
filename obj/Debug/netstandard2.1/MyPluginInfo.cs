53b2c40cfae981d18bfc5f393987bfd476fcb18e
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-01-12 14:45:26 -0800

    Fixing The Repo

---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace VirtualInsanity
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "justsunnit.virtual_insanity";
+        public const string PLUGIN_NAME = "VirtualInsanity";
+        public const string PLUGIN_VERSION = "0.2.0";
+    }
+}
