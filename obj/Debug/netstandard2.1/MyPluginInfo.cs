988436e0c32ad47fa0e8057cf61f3d968e25d45a
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-01-13 18:46:12 -0800

    0.2.5

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -4,6 +4,6 @@ namespace VirtualInsanity
     {
         public const string PLUGIN_GUID = "justsunnit.virtual_insanity";
         public const string PLUGIN_NAME = "VirtualInsanity";
-        public const string PLUGIN_VERSION = "0.1.0";
+        public const string PLUGIN_VERSION = "0.2.5";
     }
 }
